3e16924c1d8526e0a0af75bd0e92302bc6332c7a
Elar Saks <elarsaks@gmail.com>
2024-05-02 10:17:06 +0300

    Finnish with basics recap.

--- src/main/scala/lectures/part1as/Recap.scala
+++ src/main/scala/lectures/part1as/Recap.scala
@@ -44,4 +44,73 @@ object Recap extends App {
   aCroc.eat(aDog)
   aCroc eat aDog // natural language
 
+  // anonymous classes
+  val aCarnivore = new Carnivore {
+    override def eat(a: Animal): Unit = println("Roar!")
+  }
+
+  // generics
+  abstract class MyList[+A] // variance and variance problems in THIS course
+  // singletons and companions
+  object MyList
+
+  // case classes
+  case class Person(name: String, age:Int)
+
+  // exceptions and try/catch/finally
+  val throwsException = throw new RuntimeException // Nothing
+  val aPotentialFailure = try {
+    throw new RuntimeException()
+  }  catch {
+    case e: Exception => "I caught an exception"
+  } finally {
+    println("some logs")
+  }
+
+  // packaging and imports
+
+  // functional programming
+  val incrementer = new Function1[Int, Int]{
+    override def apply(v1: Int): Int = v1 + 1
+  }
+
+  incrementer(1)
+
+  val anonymousIncrementer = (x: Int) => x + 1
+  List(1,2,3).map(anonymousIncrementer) // HOF
+  // map, flatMap, filter
+
+  // for-comprehension
+  val pairs = for {
+    num <- List(1,2,3)
+    char <- List('a', 'b', 'c')
+  } yield num + "-" + char
+
+  // Scala collections: Seqs, Arrays, Lists, Vectors, Maps, Tuples
+  val aMap = {
+    "Daniel" -> 789,
+    "Jess" -> 555
+  }
+
+  // "collections": Options, Try
+  val anOption = Some(2)
+
+  // pattern matching
+  val x = 2
+  val order = x match {
+    case 1 => "first"
+    case 2 => "second"
+    case 3 => "third"
+    case _ => x + "th"
+  }
+
+  val bob = Person("Bob", 22)
+  val greeting = bob match {
+    case Person(n, _) => s"Hi, my name is $n"
+  }
+
+  // all the patterns
 }
+
+
+
